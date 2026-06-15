ffa18c35dc12e658aa6c618ce600bab8ed79af51
Jeremy Weiss <jlweiss@email.arizona.edu>
2026-06-15 16:07:21 -0700

    update error modal

--- app/R/scr03_datepickerErrorModal.R
+++ app/R/scr03_datepickerErrorModal.R
@@ -1,16 +1,16 @@
 datepickerErrorModal <- 
   shiny::modalDialog(
-    shiny::em(
-      "Please select a 'Start Date' that is earlier than or the same as the 'End Date'."
+    htmltools::HTML(
+      "<em>Please select a <b>Start Date</b> that is earlier than or the same as the <b>End Date</b>.</em>"
     ),
     easyClose = FALSE,
-    fade = TRUE,
+    fade = FALSE,
     footer = shiny::modalButton("CLOSE"),
     size = "s",
-    title = htmltools::p(
-      id = "datepickerModal",
-      bsicons::bs_icon("sliders"), 
-      htmltools::HTML("&nbsp;"),
-      "DATA OPTIONS"
-    )
+    title = 
+      htmltools::p(
+        id = "datepickerModal",
+        bsicons::bs_icon("sliders", class = "bolder-icon"), 
+        htmltools::HTML("&nbsp;<b>DATA OPTIONS</b>")
+      )
   )
